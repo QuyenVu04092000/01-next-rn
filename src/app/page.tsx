354f6a031f67af9f7176ea6766accf75dda8daf1
Quyen <vuquyen.04092000@gmail.com>
2024-10-15 00:41:28 +0700

    init project

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from "antd";
+
+
+export default function Home() {
+  return (
+    <div>hello world
+      <div>
+        <Button type="primary">
+          Add new
+        </Button>
+      </div>
+    </div>
+  );
+}
